974548036bcb49be8b7cc20bedf48053a27968d1
Iga Ratajczak <iga.ratajczak1502@gmail.com>
2020-05-30 23:13:17 +0200

    up

---
+++ zad_2_15.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(){
+    long liczba;
+    int podstawa;
+    cout<<"Podaj liczbe w systemie dziesietnym oraz podstawe systemu na kotry ma zostac przekonwertowana"<<endl;
+    cin>>liczba>>podstawa;
+    string pom="0123456789ABCDEF";
+    string temp, wynik;
+    while(liczba!=0){
+        temp+=pom[liczba%podstawa];
+        liczba=liczba/podstawa;
+    }
+    for(int i=temp.size()-1; i>=0; i--){
+        wynik+=temp[i];
+    }
+    cout<<"Liczba przekonwertowana w systemie "<<podstawa<<" to "<<wynik;
+    return 0;
+}
